f0450b3783bfa81010742759c7a5664450e083d5
LeRoiDesKiwis <lekiwidu17@gmail.com>
2020-05-12 16:05:21 +0200

    first commit

--- Example/src/main/java/fr/leroideskiwis/client/Vars.java
+++
@@ -1,7 +0,0 @@
-package fr.leroideskiwis.client;
-
-public class Vars {
-
-    public static String token = "NzA5NzI2OTM5MjU0MDk1ODky.XrqGwQ.DQTEPW4b9MJxjgWfCYsuLFkl4UE";
-
-}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Disvacord
+Disvacord is a discord library for java developers.
+
+# How to use
+You can see examples on the folder Examples.
